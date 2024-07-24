8150e5469b3a9c0e064bad16725ead6ddd416b28
DavidAdler <adlerdovi@gmail.com>
2024-07-24 11:49:10 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace HebrewDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(712, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 25);
+            label1.TabIndex = 0;
+            label1.Text = "היום בשבוע";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(503, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 25);
+            label2.TabIndex = 1;
+            label2.Text = "היום בחודש";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(277, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 25);
+            label3.TabIndex = 2;
+            label3.Text = "חודש";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(67, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 25);
+            label4.TabIndex = 3;
+            label4.Text = "שנה";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "ראשון", "שני", "שלישי", "רביעי", "חמישי", "ששי" });
+            comboBox1.Location = new Point(653, 130);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" });
+            comboBox2.Location = new Point(433, 130);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(182, 33);
+            comboBox2.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "תשרי", "מרחשון", "כסלו", "טבת", "שבט", "אדר", "אדר הראשון", "אדר השני", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול" });
+            comboBox3.Location = new Point(215, 130);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(182, 33);
+            comboBox3.TabIndex = 6;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Items.AddRange(new object[] { "תשפ\"ד", "תשפ\"ה", "תשפ\"ו", "תשפ\"ז", "תשפ\"ח", "תשפ\"ט", "תש\"צ", "תשצ\"א", "תשצ\"ב" });
+            comboBox4.Location = new Point(8, 130);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(182, 33);
+            comboBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(404, 331);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 8;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(880, 450);
+            Controls.Add(button1);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+namespace HebrewDate
+{
+
+    public partial class Form1 : Form
+    {
+        private const string PATH = ".//dates.xml";
+        private XDocument _activeDoc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            EnsureFile();
+            _activeDoc = XDocument.Load(PATH);
+
+        }
+        private void EnsureFile()
+        {
+            if (!File.Exists(PATH))
+            {
+                var root = new XDocument(new XElement("Queries"));
+                root.Save(PATH);
+            }
+        }
+
+        private string GenerateMessageDayMonth()
+        {
+            string dayMonth = comboBox2.SelectedItem.ToString();
+            string dayMonthMessage = dayMonth switch
+            {
+                "1" => "יום אחד",
+                "2" => "שני ימים",
+                "3" => "שלשה ימים",
+                "4" => "ארבעה ימים",
+                "5" => "חמשה ימים",
+                "6" => "ששה ימים",
+                "7" => "שבעה ימים",
+                "8" => "שמנה ימים",
+                "9" => "תשעה ימים",
+                "10" => "עשרה ימים",
+                "11" => "אחד עשר יום",
+                "12" => "שנים עשר יום",
+                "13" => "שלשה עשר יום",
+                "14" => "ארבעה עשר יום",
+                "15" => "חמשה עשר יום",
+                "16" => "ששה עשר יום",
+                "17" => "שבעה עשר יום",
+                "18" => "שמנה עשר יום",
+                "19" => "תשעה עשר יום",
+                "20" => "עשרים יום",
+                "21" => "אחד ועשרים יום",
+                "22" => "שנים ועשרים יום",
+                "23" => "שלשה ועשרים יום",
+                "24" => "ארבעה ועשרים יום",
+                "25" => "חמשה ועשרים יום",
+                "26" => "ששה ועשרים יום",
+                "27" => "שבעה ועשרים יום",
+                "28" => "שמנה ועשרים יום",
+                "29" => "תשעה ועשרים יום",
+                "30" => "יום שלשים לחדש פלוני שהוא ראש חדש פלוני"
+            };
+            return dayMonthMessage;
+        }
+        
+        private string GenerateYearMessage()
+        {
+            string year = comboBox4.SelectedItem.ToString();
+            string yearMessage = year switch
+            {
+                "תשפ\"ד" => "שמנים וארבע",
+                "תשפ\"ה" => "שמנים וחמש",
+                "תשפ\"ן" => "שמנים ושש",
+                "תשפ\"ז" => "שמנים ושבע",
+                "תשפ\"ח" => "שמנים ושמנה",
+                "תשפ\"ט" => "שמנים ותשע",
+                "תש\"צ" => "תשעים",
+                "תשצ\"א" => "תשעים ואחת",
+                "תשצ\"ב" => "תשעים ושתים",
+            };
+            return yearMessage;
+        }
+        private  string Result()
+        {
+            string dayMessage = comboBox1.SelectedItem.ToString();
+            string dayMonthMessage = GenerateMessageDayMonth();
+            string yearMessage = GenerateYearMessage();
+            string month = comboBox3.SelectedItem.ToString();
+            string result = $"לבריאת העולם {yearMessage}בשנת חמשת אלפים ושבע מאות ו {month} לירח {dayMonthMessage} {dayMessage}ב";
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XElement root = _activeDoc.Root;
+            XElement dayOfWeek = new XElement("Day", comboBox1.SelectedItem.ToString());
+            XElement dayOfMonth = new XElement("DayMonth", comboBox2.SelectedItem.ToString());
+            XElement monthName = new XElement("Month", comboBox3.SelectedItem.ToString());
+            XElement year = new XElement("Year", comboBox4.SelectedItem.ToString());
+            XElement result = new XElement("Result", Result());
+            XElement query = new XElement("Query", dayOfWeek, dayOfMonth, monthName, year, result);
+            root.Add(query);
+            _activeDoc.Save(PATH);
+
+            MessageBox.Show(Result());
+        }
+    }
+}
